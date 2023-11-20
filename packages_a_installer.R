0bf957785c6d27a3da7e456988edd950503c3cd6
jean-seb-2106 <jean.sebban@insee.fr>
2023-11-20 13:19:35 +0000

    maj des packages

--- packages_a_installer.R
+++ packages_a_installer.R
@@ -1,6 +1,6 @@
 # packages à installer quand on démarre Onyxia
 
-install.packages(c("questionr","report","kableExtra"))
+install.packages(c("questionr","report","kableExtra","BioStatR"))
 
 
 #Pour ne pas rentrer le mot de passe dans GIT à chaque fois
